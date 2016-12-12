c37d86e20243436b32034d289cb219817d3766d9
kubekin2 <vkubekin@gmail.com>
2016-12-12 03:46:22 +0300

    Update 2016.3

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ var Stimulsoft = require('./stimulsoft.reports');
 Stimulsoft.System.Drawing.Graphics.opentypeClass = require('opentype.js');
 
 module.exports = Stimulsoft;
+Stimulsoft.System.NodeJs.useWebKit = false;
 Stimulsoft.System.NodeJs.initialize();
