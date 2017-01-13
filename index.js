67f1fad51aec98e429cb5cda92e06d4dc50fd5c0
kubekin2 <vkubekin@gmail.com>
2017-01-13 13:17:57 +0300

    Upload 2013.3.4

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 Element = 'undefined';
 var Stimulsoft = require('./stimulsoft.reports');
+JSZip = require('jszip');
 Stimulsoft.System.Drawing.Graphics.opentypeClass = require('opentype.js');
 
 module.exports = Stimulsoft;
