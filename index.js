8bebb29483cc04b876515a03f6026457922262c5
kubekin2 <vkubekin@gmail.com>
2019-04-05 14:47:15 +0300

    2019.2.3

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-Element = 'undefined';
-moment_mod = require('moment');
 var Stimulsoft = require('./stimulsoft.reports');
-JSZip = require('jszip');
 xmldoc = require('xmldoc');
-XLSX = require('xlsx');
-XXH = global["XXH"];
-Stimulsoft.System.Drawing.Graphics.opentypeClass = require('opentype.js');
 
 module.exports = Stimulsoft;
 Stimulsoft.System.NodeJs.useWebKit = false;
