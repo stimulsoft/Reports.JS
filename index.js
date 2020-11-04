12939bf9e7e382855e5f3cbb8ed23d9ba56c6824
Yuriy Kubekin <vkubekin@gmail.com>
2020-11-04 17:46:27 +0300

    2020.5.1.1

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var Stimulsoft = require('./Scripts/stimulsoft.reports').Stimulsoft;
 Stimulsoft.ExternalLibrary.xmldoc = require('xmldoc');
 
-module.exports = Stimulsoft;
 Stimulsoft.System.NodeJs.useWebKit = false;
 Stimulsoft.System.NodeJs.initialize();
+
+module.exports = Stimulsoft;
