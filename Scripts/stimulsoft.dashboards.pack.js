2aa533c577c72002ff69b9a4f945cf4ead662ff5
Yuriy Kubekin <vkubekin@gmail.com>
2025-03-20 16:47:56 +0100

    v2025.2.1

--- Scripts/stimulsoft.dashboards.pack.js
+++ Scripts/stimulsoft.dashboards.pack.js
@@ -1,1 +1,11 @@
-!function(e){'object'==typeof exports&&'undefined'!=typeof module?module.exports=((s)=>Object.assign(s,e(s.Stimulsoft)))(require('./stimulsoft.reports.pack')):'function'==typeof define&&define.amd?define(['./stimulsoft.reports.pack'],(s)=>Object.assign(s,e(s.Stimulsoft))):Object.assign(window,e(window.Stimulsoft))}(function(Stimulsoft){if(Stimulsoft){if(Stimulsoft.__engineVersion&&Stimulsoft.__engineVersion!=='2025.1.6')console.warn('Scripts versions mismatch: engine ver. = %s; dashboards ver. = 2025.1.6', Stimulsoft.__engineVersion);else if(Stimulsoft.__reportsVersion!=='2025.1.6')console.warn('Scripts versions mismatch: reports ver. = %s; dashboards ver. = 2025.1.6', Stimulsoft.__reportsVersion);}return{Stimulsoft:Stimulsoft||{}};});
+!function(e){
+        'object'==typeof exports&&'undefined'!=typeof module?module.exports=((s)=>Object.assign(s,e(s.Stimulsoft)))(require('./stimulsoft.reports.pack'))
+        :'function'==typeof define&&define.amd?define(['./stimulsoft.reports.pack'],(s)=>Object.assign(s,e(s.Stimulsoft))):Object.assign(window,e(window.Stimulsoft))}
+        (function(Stimulsoft){
+        if(Stimulsoft){
+        if(Stimulsoft.__engineVersion&&Stimulsoft.__engineVersion!=='2025.2.1')
+        console.warn('Scripts versions mismatch: engine ver. = %s; dashboards ver. = 2025.2.1', Stimulsoft.__engineVersion);
+        else if(Stimulsoft.__reportsVersion!=='2025.2.1')
+        console.warn('Scripts versions mismatch: reports ver. = %s; dashboards ver. = 2025.2.1', Stimulsoft.__reportsVersion);
+        }
+        return{Stimulsoft:Stimulsoft||{}};});
