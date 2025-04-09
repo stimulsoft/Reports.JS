9bb04e9ee6425ed14c24daed2616a5e8a182614e
Yuriy Kubekin <vkubekin@gmail.com>
2025-04-09 09:53:57 +0200

    v2025.2.2

--- Scripts/stimulsoft.dashboards.pack.js
+++ Scripts/stimulsoft.dashboards.pack.js
@@ -3,9 +3,9 @@
         :'function'==typeof define&&define.amd?define(['./stimulsoft.reports.pack'],(s)=>Object.assign(s,e(s.Stimulsoft))):Object.assign(window,e(window.Stimulsoft))}
         (function(Stimulsoft){
         if(Stimulsoft){
-        if(Stimulsoft.__engineVersion&&Stimulsoft.__engineVersion!=='2025.2.1')
-        console.warn('Scripts versions mismatch: engine ver. = %s; dashboards ver. = 2025.2.1', Stimulsoft.__engineVersion);
-        else if(Stimulsoft.__reportsVersion!=='2025.2.1')
-        console.warn('Scripts versions mismatch: reports ver. = %s; dashboards ver. = 2025.2.1', Stimulsoft.__reportsVersion);
+        if(Stimulsoft.__engineVersion&&Stimulsoft.__engineVersion!=='2025.2.2')
+        console.warn('Scripts versions mismatch: engine ver. = %s; dashboards ver. = 2025.2.2', Stimulsoft.__engineVersion);
+        else if(Stimulsoft.__reportsVersion!=='2025.2.2')
+        console.warn('Scripts versions mismatch: reports ver. = %s; dashboards ver. = 2025.2.2', Stimulsoft.__reportsVersion);
         }
         return{Stimulsoft:Stimulsoft||{}};});
